46fa63948cedba7575d52870ec52f5702421ca1d
yuri-anjos <yuri.dos.4njos@gmail.com>
2024-06-08 11:28:16 -0300

    str-producer started

---
+++ str-producer/src/test/java/com/yuri/strproducer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuri.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
