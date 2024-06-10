4cd3900ad0ad54d15c222dd8203569b2384b2ea5
yuri-anjos <yuri.dos.4njos@gmail.com>
2024-06-10 17:12:14 -0300

    definindo partições para consumir

--- str-consumer/src/main/java/com/yuri/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/yuri/strconsumer/listeners/StrConsumerListener.java
@@ -3,6 +3,7 @@ package com.yuri.strconsumer.listeners;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,17 +11,25 @@ public class StrConsumerListener {
 
 	private final Logger logger = LoggerFactory.getLogger(StrConsumerListener.class);
 
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory",
+			topicPartitions = {
+					@TopicPartition(topic = "str-topic", partitions = {"0"})
+			}
+	)
 	public void create(String message) {
 		logger.info("CREATE ::: Receive message \n{}", message);
 	}
 
-	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory",
+			topicPartitions = {
+					@TopicPartition(topic = "str-topic", partitions = {"1"})
+			}
+	)
 	public void log(String message) {
 		logger.info("LOG ::: Receive message \n{}", message);
 	}
 
-	@KafkaListener(groupId = "group-3", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
 	public void email(String message) {
 		logger.info("Email ::: Receive message \n{}", message);
 	}
